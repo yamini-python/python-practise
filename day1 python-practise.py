d53a1a9c45fac9e5ddbf49cf259e9903bdb06e6f
yamini-python <79084426+yamini-python@users.noreply.github.com>
2021-02-15 17:14:55 +0530

    Add files via upload

---
+++ day1 python-practise.py
@@ -0,0 +1,49 @@
+#1
+names = ["john", "jake", "jack", "jack","george", "jenny", "jason"]
+unique=set(names)
+print("unique names:",unique)
+for name in unique:
+    if((len(name)<5) and ('e' not in name)):
+         print(name)
+
+#2
+string ="python"
+print('c'+string[1:])
+
+
+#3
+new={"name": "python", "ext": "py", "creator": "guido"}
+keys=list(new.keys())
+value=list(new.values())
+both=list(new.items())
+print("keys:",keys)
+print("values:",value)
+print("both:",both)
+
+
+#4 fizzbuzz
+for n in range(5):
+    num=int(input("Enter the number"))
+    if num % 3 == 0 and num % 5 == 0:
+        print("fizzbuzz")
+    elif num % 3 == 0:
+        print("fizz")
+    elif num % 5 == 0:
+        print("buzz")
+    else:
+        print("nothing")
+
+
+#5 guess the number
+
+import random
+guess=random.randint(1,11)
+num=int(input("Enter the number"))
+while(guess!=num):
+    if(num<guess):
+        print("your guess value is lower than the real value")
+        num=int(input("Enter the number"))
+    elif(num>guess):
+        print("your guess value is higher than the real value")
+        num=int(input("Enter the number"))
+print("correct answer")
